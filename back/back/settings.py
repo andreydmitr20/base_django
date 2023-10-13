92932a627284f5be15fe8dd18d63f07680331d0d
andreydmitr21 <andreydmitr20@gmail.com>
2023-10-13 19:41:57 +0200

    psycopg2-binary

--- back/back/settings.py
+++ back/back/settings.py
@@ -110,22 +110,22 @@ WSGI_APPLICATION = "back.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': env("db_name),
-#         'USER': env("db_user"),
-#         'PASSWORD': env("db_pass"),
-#         'HOST': env("db_host"),
-#         'PORT': 5432,
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("db_name"),
+        "USER": env("db_user"),
+        "PASSWORD": env("db_pass"),
+        "HOST": env("db_host"),
+        "PORT": env("db_port"),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
